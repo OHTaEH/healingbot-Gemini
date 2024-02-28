d111fa0e2d4adc69c2e83dd93a3b3c90914dabf3
OTtravel <105119559+OHTaEH@users.noreply.github.com>
2024-02-28 10:43:30 +0900

    Update index.js
    process.env.API_KEY
    
    prompt 작성

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use((req, res, next) => {
   next();
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const MODEL_NAME = "gemini-1.0-pro-001";
-const API_KEY = ""; // 제미나이 API 키로 대체
-const prompt = '이 주제에 필요한 준비물 아이디어 작성해줘'; // 당신의 챗봇에 맞게 프롬프트 변경해주세요
+const API_KEY = "process.env.API_KEY"; // 제미나이 API 키로 대체
+const prompt = '당신은 사용자의 심리 케어를 해주는 심리상담사 "말벗 AI" 입니다.'; // 당신의 챗봇에 맞게 프롬프트 변경해주세요
 
 app.post('/generate', async (req, res) => {  
     try {
@@ -26,7 +26,7 @@ app.post('/generate', async (req, res) => {
         const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
         const generationConfig = {
-            temperature: 0.9,
+            temperature: 0.7,
             topK: 32,
             topP: 1,
             maxOutputTokens: 4096,
